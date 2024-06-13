000807443715b8409a5a62d5a32bdd477cb25380
andreybalabukha <5344208@gmail.com>
2024-06-13 10:23:49 +0800

    [EPMGCIPGAB-66] create boilerplate for the i18n

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const t = useTranslations("Index");
   return (
     <main>
-      <p>uz-gabt-webapp {t('title')}</p>
+      <p>uz-gabt-webapp  {t('title')}</p>
     </main>
   );
 }

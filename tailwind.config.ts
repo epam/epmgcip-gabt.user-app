18f29166a4d167dc731eb37c25ee8f5d88922aa0
vladyffka <vzalesinski@gmail.com>
2024-08-14 12:38:16 +0400

    refactor: change tabs to spaces

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,27 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
-	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-				'gradient-conic':
-					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-			}
-		},
-		colors: {
-			'dark-red': '#A80403',
-			'light-yellow': '#F9EED7',
-			black: '#160807',
-			'warm-orange': '#E7B24C',
-			white: '#FFF'
-		}
-	},
-	plugins: []
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+      }
+    },
+    colors: {
+      'dark-red': '#A80403',
+      'light-yellow': '#F9EED7',
+      black: '#160807',
+      'warm-orange': '#E7B24C',
+      white: '#FFF'
+    }
+  },
+  plugins: []
 }
 export default config

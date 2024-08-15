2db6d87e4d54f931d288339716d4f7ec1a38c4cc
vladyffka <vzalesinski@gmail.com>
2024-08-15 14:46:10 +0400

    fix: fix url in mock data

--- src/constants/next-events-mock.ts
+++ src/constants/next-events-mock.ts
@@ -33,7 +33,7 @@ const formattedTime: string = `${now.getHours().toString().padStart(padToStartNu
 export const NEXT_EVENTS: INextEvent[] = [
   {
     type: EventCategory.Opera,
-    img: "/public/mock-data/mock-next-event-img.jpg",
+    img: "/mock-data/mock-next-event-img.jpg",
     title:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
     text: "Malesuada pellentesque elit eget gravida cum sociis. Pretium viverra suspendisse potenti nullam ac tortor. Pharetra et ultrices neque ornare aenean euismod elementum nisi. Id eu nisl nunc mi ipsum.elis imperdiet proin fermentum leo. Quisque id diam vel quam elementum pulvinar etiam non. Purus semper eget duis at tellus. Lacinia at quis risus sed vulputate odio ut enim. Tincidunt nunc pulvinar sapien et. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Sit amet cursus sit amet. Ac auctor augue mauris augue neque gravida in fermentum. Ut eu sem integer vitae justo eget. Orci sagittis eu volutpat odio facilisis mauris sit amet massa. Sit amet mattis vulputate enim. Viverra nam libero justo laoreet.",
@@ -43,7 +43,7 @@ export const NEXT_EVENTS: INextEvent[] = [
   },
   {
     type: EventCategory.Ballet,
-    img: "/public/mock-data/mock-next-event-img.jpg",
+    img: "/mock-data/mock-next-event-img.jpg",
     title:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
     text: "Malesuada pellentesque elit eget gravida cum sociis. Pretium viverra suspendisse potenti nullam ac tortor. Pharetra et ultrices neque ornare aenean euismod elementum nisi. Id eu nisl nunc mi ipsum. Ut lectus arcu bibendum at varius vel pharetra vel turpis. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Luctus accumsan tortor posuere ac. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Quisque id diam vel quam elementum pulvinar etiam non. Purus semper eget duis at tellus. Lacine enim. Viverra nam libero justo laoreet.",
@@ -53,7 +53,7 @@ export const NEXT_EVENTS: INextEvent[] = [
   },
   {
     type: EventCategory.Childrens,
-    img: "/public/mock-data/mock-next-event-img.jpg",
+    img: "/mock-data/mock-next-event-img.jpg",
     title:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
     text: "Malesuada pellentesque elit eget gravida cum sociis. Pretium viverra suspendisse potenti nullam ac tortor. Pharetra et ultrices neque ornare aenean euismod elementum nisi. Id eu nisl nunc mi ipsum. Ut lectus arcu bibendum at varius vel pharetra vel turpis. Faucibus pulvinar elementum integer enim neque volutpat ac tincidunt. Luctus accumsan tortor posuere ac. In metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Quisque id diam vel quam elementum pulvinar etiam non. Purus semper eget duis at tellus. Lacinia at quis risus sed vulputate odio ut enim. Tincidunt nunc pulvinar sapien et. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Sit amet cursus sit amet. Ac auctor augue mauris augue neque gravidu scelerisque felis imperdiet proin fermentum leo. Quisque id diam vel quam elementum pulvinar etiam non. Purus semper eget duis at tellus. Lacinia at quis risus sed vulputate odio ut enim. Tincidunt nunc pulvinar sapien et. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Sit amet cursus sit amet. Ac auctor augue mauris augue neque gravida in fermentum. Ut eu sem integer vitae justo eget. Orci sagittis eu volutpat odio facilisis mauris sit amet massa. Sit amet mattis vulputate enim. Viverra nam libero justo laoreet.",
@@ -63,7 +63,7 @@ export const NEXT_EVENTS: INextEvent[] = [
   },
   {
     type: EventCategory.Concerts,
-    img: "/public/mock-data/mock-next-event-img.jpg",
+    img: "/mock-data/mock-next-event-img.jpg",
     title:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
     text: "Malesuada pellentesavida in fermentum. Ut eu sem integer vitae justo eget. Orci sagittis eu volutpat odio facilisis mauris sit amet massa. Sit amet mattis vulputate enim. Viverra nam libero justo laoreet.",
@@ -73,7 +73,7 @@ export const NEXT_EVENTS: INextEvent[] = [
   },
   {
     type: EventCategory.Musical,
-    img: "/public/mock-data/mock-next-event-img.jpg",
+    img: "/mock-data/mock-next-event-img.jpg",
     title:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
     text: "Malesuada pellentesavida in fermentum. Ut eu sem integer vitae justo eget. Orci sagittis eu volutpat odio facilisis mauris sit amet massa. Sit amet mattis vulputate enim. Viverra nam libero justo laoreet.",
@@ -83,7 +83,7 @@ export const NEXT_EVENTS: INextEvent[] = [
   },
   {
     type: EventCategory.Tours,
-    img: "/public/mock-data/mock-next-event-img.jpg",
+    img: "/mock-data/mock-next-event-img.jpg",
     title:
       "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut",
     text: "Malesuada pellentesavida in fermentum. Ut eu sem integer vitae justo eget. Orci sagittis eu volutpat odio facilisis mauris sit amet massa. Sit amet mattis vulputate enim. Viverra nam libero justo laoreet.",

4f3fdc2b35db29e158bdf0b694ec7790e6feda10
suren <tadevosyansuren07@gmail.com>
2024-08-19 12:56:25 +0400

    Fix: Converted line endings

--- src/components/buttons/menu-buttn/index.tsx
+++ src/components/buttons/menu-buttn/index.tsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 import openMenu from "@/public/openBar.svg";
 import closeMenu from "@/public/Close.svg";
 
+import {
+  MENU_BUTTON_ALT,
+  MENU_BUTTON_TEXT,
+} from "@/src/constants/menu-button-literals";
 import { IMenuButtonProps } from "../menu-buttn/types";
-import { MENU_BUTTON_ALT, MENU_BUTTON_TEXT } from "@/src/constants/menu-button-literals";
 
 const MenuButton: React.FC<IMenuButtonProps> = ({
   isOpen,
--- src/constants/menu-button-literals.tsx
+++ src/constants/menu-button-literals.tsx
@@ -1,15 +1,14 @@
 interface IMenuButtonLiterals {
-    OPEN: string;
-    CLOSE: string;
-  }
-  
-  export const MENU_BUTTON_TEXT: IMenuButtonLiterals = {
-    OPEN: "MENU",
-    CLOSE: "Close",
-  };
-  
-  export const MENU_BUTTON_ALT: IMenuButtonLiterals = {
-    OPEN: "Open menu",
-    CLOSE: "Close menu",
-  };
-  
+  OPEN: string;
+  CLOSE: string;
+}
+
+export const MENU_BUTTON_TEXT: IMenuButtonLiterals = {
+  OPEN: "MENU",
+  CLOSE: "Close",
+};
+
+export const MENU_BUTTON_ALT: IMenuButtonLiterals = {
+  OPEN: "Open menu",
+  CLOSE: "Close menu",
+};
